3436c13e438ffd554f561d7a1c84e5d14e3637b3
Ryan State <rwstate@gmail.com>
2020-01-07 17:50:46 -0500

    update login/logout routes

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,41 +22,33 @@ router.post("/register", (req, res) => {
 router.post("/login", (req, res) => {
   const creds = req.body;
 
-  if (!req.session.name) {
-    Users.checkCreds(creds)
-    .then(user => {
-      if (user && bcrypt.compareSync(creds.password, user.password)) {
-        req.session.name = user.id;
-        res
-          .status(200)
-          .json({ msg: "Login successful", user_id: user.id });
-      } else {
-        res.status(401).json({ msg: "Invalid credentials" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ errMsg: "error validating user" });
-    });
-  } else {
-    res.status(400).json({errMsg: "already logged in"})
-  }
+  Users.checkCreds(creds)
+  .then(user => {
+    if (user && bcrypt.compareSync(creds.password, user.password)) {
+      req.session.name = user.id;
+      res
+        .status(200)
+        .json({ msg: "Login successful", user_id: user.id });
+    } else {
+      res.status(401).json({ msg: "Invalid credentials" });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ errMsg: "error validating user" });
+  });
+
 
   
 });
 
 router.get("/logout", (req, res) => {
-  if (!req.session.name) {
-    res.status(400).json({ errMsg: "already logged out" });
-  } else {
-    req.session.destroy(err => {
-      if (err) {
-        res.json({ errMsg: "error logging out" });
-      } else {
-        res.send({ msg: "user logged out" });
-      }
-    });
-  }
-  
+  req.session.destroy(err => {
+    if (err) {
+      res.json({ errMsg: "error logging out" });
+    } else {
+      res.send({ msg: "user logged out" });
+    }
+  }); 
 });
 
 module.exports = router;
